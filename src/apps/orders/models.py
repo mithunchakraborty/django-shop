3d1c7944f039ebef0f5030db437594e0132afd12
mithunchakraborty <motyletsdenis@gmail.com>
2020-08-26 00:33:09 +0700

    end orders basics

--- src/apps/orders/models.py
+++ src/apps/orders/models.py
@@ -4,13 +4,12 @@ from shop.models import Product
 
 class Order(models.Model):
     """
+    Order class.
     """
     first_name = models.CharField(max_length=50)
     last_name= models.CharField(max_length=50)
     email = models.EmailField()
     address = models.CharField(max_length=250)
-
-    #
     postal_code = models.CharField(max_length=20)
     city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
@@ -28,4 +27,23 @@ class Order(models.Model):
 
     def get_global_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    """
+    Order item class.
+    """
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        db_table = 'order_item'
+
+    def __str__(self):
+        return f'{self.id}'
+
+    def get_cost(self):
+        return self.price * self.quantity
 
