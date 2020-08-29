5c56bd925325ece1b7c30f19259c58922c3ccd54
mithunchakraborty <motyletsdenis@gmail.com>
2020-08-29 19:45:49 +0700

    create coupons app

--- src/apps/orders/urls.py
+++ src/apps/orders/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'orders'
 urlpatterns = [
     path('create/', views.create_order_view, name='create_order_view'),
+    path(
+        'admin/order/<int:order_id>', 
+        views.admin_order_detail_view, 
+        name='admin_order_detail_view'
+    )
 ]
 
--- src/apps/orders/views.py
+++ src/apps/orders/views.py
@@ -1,9 +1,11 @@
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .services import send_email_if_order_created
+from .models import Order
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def create_order_view(request):
@@ -44,4 +46,17 @@ def create_order_view(request):
             'orders/order/create.html',
             {'cart': cart, 'form': form}
         )
+
+
+@staff_member_required
+def admin_order_detail_view(request, order_id):
+    """
+    """
+    order = get_object_or_404(Order, id=order_id)
+
+    return render(
+        request,
+        'admin/orders/order/detail.html',
+        {'order': order}
+    )
 
